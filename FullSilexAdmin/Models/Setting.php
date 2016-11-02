85e6d8e6a44d85781fc9ff683a24887572e7b0fe
Bobby Stenly Irawan <iceman.bsi@gmail.com>
2016-11-02 14:38:35 +0700

    setting admin

---
+++ FullSilexAdmin/Models/Setting.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Trio-1602
+ * Date: 11/2/16
+ * Time: 2:05 PM
+ */
+
+namespace FullSilexAdmin\Models;
+
+use FullSilex\Models\BaseModel;
+use FullSilex\Libraries\EncryptionClass\EncryptionClass;
+
+class Setting extends BaseModel
+{
+    static $table_name = "setting";
+
+    static $before_save = array('beforeSave');
+    static $before_create = array('beforeCreate');
+
+    public function beforeCreate(){
+        $lastSetting = Setting::first(array(
+            "order" => 'position desc'
+        ));
+        if(!empty($lastSetting)){
+            $pos = $lastSetting->position + 1;
+        }
+        else{
+            $pos = 1;
+        }
+        $this->position = $pos;
+    }
+
+    public function beforeSave(){
+        $crypt = new EncryptionClass();
+        if ($this->input_type == "password") {
+            $this->value = $crypt->encrypt($this->app->config('globalSalt'), $this->value);
+        }
+    }
+}
